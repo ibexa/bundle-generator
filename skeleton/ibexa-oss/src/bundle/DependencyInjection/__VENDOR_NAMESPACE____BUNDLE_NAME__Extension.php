06bc33176a1cb3777a12fd36c5043aa9b02846c4
Adam Wójs <adam@wojs.pl>
2023-03-05 14:42:33 +0100

    Added prepended configuration to oss/ee skeletons

--- skeleton/ibexa-oss/src/bundle/DependencyInjection/__VENDOR_NAMESPACE____BUNDLE_NAME__Extension.php
+++ skeleton/ibexa-oss/src/bundle/DependencyInjection/__VENDOR_NAMESPACE____BUNDLE_NAME__Extension.php
@@ -9,11 +9,14 @@ declare(strict_types=1);
 namespace __VENDOR_NAMESPACE__\Platform\Bundle\__BUNDLE_NAME__\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Yaml\Yaml;
 
-final class __VENDOR_NAMESPACE____BUNDLE_NAME__Extension extends Extension
+final class __VENDOR_NAMESPACE____BUNDLE_NAME__Extension extends Extension implements PrependExtensionInterface
 {
     /**
      * @param array<string, mixed> $configs
@@ -26,5 +29,15 @@ final class __VENDOR_NAMESPACE____BUNDLE_NAME__Extension extends Extension
         );
 
         $loader->load('services.yaml');
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $configFile = __DIR__ . '/../Resources/config/prepend.yaml';
+
+        $container->addResource(new FileResource($configFile));
+        foreach (Yaml::parseFile($configFile, Yaml::PARSE_CONSTANT) as $name => $config) {
+            $container->prependExtensionConfig($name, $config);
+        }
     }
 }
