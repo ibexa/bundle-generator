17f94ddf1241908e39cb20d314da927681ec3380
Adam Wójs <adam@wojs.pl>
2023-04-07 09:55:55 +0200

    Skeletons improvements  (#13)

--- skeleton/ibexa-ee/src/bundle/DependencyInjection/__VENDOR_NAMESPACE____BUNDLE_NAME__Extension.php
+++ skeleton/ibexa-ee/src/bundle/DependencyInjection/__VENDOR_NAMESPACE____BUNDLE_NAME__Extension.php
@@ -9,11 +9,14 @@ declare(strict_types=1);
 namespace __VENDOR_NAMESPACE__\Bundle\__BUNDLE_NAME__\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Yaml\Yaml;
 
-final class __VENDOR_NAMESPACE____BUNDLE_NAME__Extension extends Extension
+final class __VENDOR_NAMESPACE____BUNDLE_NAME__Extension extends Extension implements PrependExtensionInterface
 {
     /**
      * @param array<string, mixed> $configs
@@ -26,5 +29,39 @@ final class __VENDOR_NAMESPACE____BUNDLE_NAME__Extension extends Extension
         );
 
         $loader->load('services.yaml');
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $this->prependDefaultConfiguration($container);
+        $this->prependJMSTranslation($container);
+    }
+
+    private function prependDefaultConfiguration(ContainerBuilder $container): void
+    {
+        $configFile = __DIR__ . '/../Resources/config/prepend.yaml';
+
+        $container->addResource(new FileResource($configFile));
+
+        $configs = Yaml::parseFile($configFile, Yaml::PARSE_CONSTANT) ?? [];
+        foreach ($configs as $name => $config) {
+            $container->prependExtensionConfig($name, $config);
+        }
+    }
+
+    private function prependJMSTranslation(ContainerBuilder $container): void
+    {
+        $container->prependExtensionConfig('jms_translation', [
+            'configs' => [
+                '__VENDOR_NAME_____PACKAGE_NAME__' => [
+                    'dirs' => [
+                        __DIR__ . '/../../',
+                    ],
+                    'excluded_dirs' => ['Behat'],
+                    'output_dir' => __DIR__ . '/../Resources/translations/',
+                    'output_format' => 'xliff',
+                ],
+            ],
+        ]);
     }
 }
--- skeleton/ibexa-oss/src/bundle/DependencyInjection/__VENDOR_NAMESPACE____BUNDLE_NAME__Extension.php
+++ skeleton/ibexa-oss/src/bundle/DependencyInjection/__VENDOR_NAMESPACE____BUNDLE_NAME__Extension.php
@@ -6,14 +6,17 @@
  */
 declare(strict_types=1);
 
-namespace __VENDOR_NAMESPACE__\Platform\Bundle\__BUNDLE_NAME__\DependencyInjection;
+namespace __VENDOR_NAMESPACE__\Bundle\__BUNDLE_NAME__\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Yaml\Yaml;
 
-final class __VENDOR_NAMESPACE____BUNDLE_NAME__Extension extends Extension
+final class __VENDOR_NAMESPACE____BUNDLE_NAME__Extension extends Extension implements PrependExtensionInterface
 {
     /**
      * @param array<string, mixed> $configs
@@ -26,5 +29,37 @@ final class __VENDOR_NAMESPACE____BUNDLE_NAME__Extension extends Extension
         );
 
         $loader->load('services.yaml');
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $this->prependDefaultConfiguration($container);
+        $this->prependJMSTranslation($container);
+    }
+
+    private function prependDefaultConfiguration(ContainerBuilder $container): void
+    {
+        $configFile = __DIR__ . '/../Resources/config/prepend.yaml';
+
+        $container->addResource(new FileResource($configFile));
+        foreach (Yaml::parseFile($configFile, Yaml::PARSE_CONSTANT) as $name => $config) {
+            $container->prependExtensionConfig($name, $config);
+        }
+    }
+
+    private function prependJMSTranslation(ContainerBuilder $container): void
+    {
+        $container->prependExtensionConfig('jms_translation', [
+            'configs' => [
+                '__VENDOR_NAME_____PACKAGE_NAME__' => [
+                    'dirs' => [
+                        __DIR__ . '/../../',
+                    ],
+                    'excluded_dirs' => ['Behat'],
+                    'output_dir' => __DIR__ . '/../Resources/translations/',
+                    'output_format' => 'xliff',
+                ],
+            ],
+        ]);
     }
 }
